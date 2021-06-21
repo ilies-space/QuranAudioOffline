c7122998c9d6045342bb43ee352ccb02c653992f
ilies ouldmenouer <63064471+ilies-space@users.noreply.github.com>
2021-06-21 16:34:53 +0100

    Update PlayList.js

--- app/screens/PlayList.js
+++ app/screens/PlayList.js
@@ -1,9 +1,54 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, Button} from 'react-native';
 export default function PlayList() {
+  const QuranList = [
+    {
+      surah: 'Fatiha',
+      reader: 'Ahmed',
+    },
+    {
+      surah: 'Moulk',
+      reader: 'Ahmed',
+    },
+    {
+      surah: 'Fatiha',
+      reader: 'Ahmed',
+    },
+    {
+      surah: 'Moulk',
+      reader: 'Ahmed',
+    },
+    {
+      surah: 'Fatiha',
+      reader: 'Ahmed',
+    },
+    {
+      surah: 'Moulk',
+      reader: 'Ahmed',
+    },
+  ];
   return (
     <View>
       <Text>PlayList</Text>
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        data={QuranList}
+        renderItem={({item}) => {
+          return (
+            <View
+              style={{
+                flexDirection: 'row',
+                padding: 10,
+              }}>
+              <View style={{flex: 1}}>
+                <Text> {item.surah} </Text>
+                <Text> {item.reader} </Text>
+              </View>
+              <Button title={'Play'} />
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
